741bb57f5953b8f0141c2a9cf3c2d8235e01b612
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2021-05-31 12:30:24 -0400

    inital non-async

---
+++ main.py
@@ -0,0 +1,34 @@
+import re
+from urllib.parse import urlparse
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://cfp.2021.djangocon.eu"
+SPEAKER_PATH = "/2021/speaker/"
+
+
+def grab_twitter(url):
+    twit_links = []
+    r = requests.get(f"{BASE_URL}{url}")
+    text = r.text
+    soup = BeautifulSoup(text, features="html.parser")
+    for link in soup("a", href=re.compile("twitter.com")):
+        twit_links.append(urlparse(link["href"]).path[1:])
+    return twit_links
+
+
+def grab_speakers(url):
+    r = requests.get(url)
+    text = r.text
+    soup = BeautifulSoup(text, features="html.parser")
+    speakers = soup.select("h3.talk-title a")
+    twit_links = []
+    for speaker in speakers:
+        if link := grab_twitter(speaker.attrs["href"]):
+            twit_links += link
+    print(",".join(twit_links))
+
+
+if __name__ == "__main__":
+    grab_speakers(f"{BASE_URL}{SPEAKER_PATH}")
